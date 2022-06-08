0b9b912540456a709b2e7188c6a4ec86faec1c41
Joke Heyndels <jo_hannah@outlook.com>
2022-06-08 09:35:48 +0000

    Add functionality to allow users to comment

--- cheatsheet_blog/models.py
+++ cheatsheet_blog/models.py
@@ -48,7 +48,7 @@ class Comment(models.Model):
     """
     post = models.ForeignKey(Post, on_delete=models.CASCADE,
                              related_name="comments")
-    name = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
     email = models.EmailField()
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
